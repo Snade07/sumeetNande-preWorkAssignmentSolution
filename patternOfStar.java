aa39ce121ad4d62fcbe6b0a589cce26d543363ae
Snade07 <99168242+Snade07@users.noreply.github.com>
2022-02-14 12:13:18 +0530

    Add files via upload

---
+++ patternOfStar.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class patternOfStar {
+	public static void main(String[]args) {
+	
+		Scanner scanner = new Scanner(System.in);
+
+     
+        System.out.println("Enter the number of rows needed to print the pattern ");
+
+        int rows = scanner.nextInt();
+        System.out.println(" Printing the pattern ");
+        
+        for (int i=1; i<=rows; i++) 
+        { 
+           
+            for (int k=rows; k>=i; k--)
+            {
+                System.out.print("*");
+            }
+            
+            for (int j=1; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+        }
+        scanner.close();
+	}
+
+}
